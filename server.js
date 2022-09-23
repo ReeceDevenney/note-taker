d20f8c26eee67fdb92b2b9348804dfd767a4dba8
Reece Devenney <reecedevenney@gmail.com>
2022-09-23 16:58:26 -0400

    added get route

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const apiRoutes = require('./routes/apiRoutes')
 
 const PORT = process.env.PORT || 3001;
 const app = express()
@@ -6,6 +7,10 @@ const app = express()
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(express.static('public'));
+
+
+app.use('/api', apiRoutes)
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port http://localhost:${PORT}/`)
